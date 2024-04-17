4ad45e505daa24319ff43771c9a4681a17a89869
sharrakutty <sharra@umich.edu>
2024-04-17 11:30:41 -0400

    update

--- style.js
+++ style.js
@@ -35,12 +35,10 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 let currentIndex = 0;
 
 function showSlide(index) {
-  // Hide all carousel items
   carouselItems.forEach(item => {
     item.style.display = 'none';
   });
 
-  // Show the slide at the specified index
   carouselItems[index].style.display = 'block';
 }
 
@@ -54,9 +52,7 @@ function previousSlide() {
   showSlide(currentIndex);
 }
 
-// Show the first slide initially
 showSlide(currentIndex);
-
-// Set up event listeners for next and previous buttons
 document.getElementById('nextBtn').addEventListener('click', nextSlide);
 document.getElementById('prevBtn').addEventListener('click', previousSlide);
+
